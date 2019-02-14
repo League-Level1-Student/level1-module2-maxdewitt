e25dbce74f848dbf4090f755943a68b0f6d3cf3d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-02-13 16:52:46 -0800

    Smurf

--- src/Smurf.java
+++ src/Smurf.java
@@ -29,16 +29,27 @@ public class Smurf {
 
 	/* Papa Smurf wears a red hat, all the others are white. */
 	public String getHatColor() {
-		// 3. Fill in this method
-		return "";
+    if (name.equalsIgnoreCase("Papa")){
+    	return "Red";
+    }
+    
+    else {
+    	return "White";
+    }
 	}
-
+		
 	/* Smurfette is the only female Smurf. */
 	public String isGirlOrBoy() {
-		// 4. Fill in this method
-		return "";
+		
+		if (name.equalsIgnoreCase("Smurfette")){
+	    	return "Female";
+	    }
+	    
+	    else {
+	    	return "Male";
 	}
 
+}
 }
 
 
--- src/SmurfRunner.java
+++ src/SmurfRunner.java
@@ -1,4 +1,33 @@
 
 public class SmurfRunner {
+	public static void main(String[] args) {
+		
+	
+Smurf handysmurf = new Smurf("Handy");
+String handyname = handysmurf.getName();
+System.out.println(handyname);
+String handyhat = handysmurf.getHatColor();
+System.out.println("Hat Color: " + handyhat);
+String handyIsGirlOrBoy = handysmurf.isGirlOrBoy();
+System.out.println(handyIsGirlOrBoy);
+System.out.println("--------------");
 
+Smurf papasmurf = new Smurf("Papa");
+String papaname = papasmurf.getName();
+System.out.println(papaname);
+String papahat = papasmurf.getHatColor();
+System.out.println("Hat Color: " + papahat);
+String papaIsGirlOrBoy = papasmurf.isGirlOrBoy();
+System.out.println(papaIsGirlOrBoy);
+System.out.println("--------------");
+
+Smurf smurfettesmurf = new Smurf("Smurfette");
+String smurfettename = smurfettesmurf.getName();
+System.out.println(smurfettename);
+String smurfettehat = smurfettesmurf.getHatColor();
+System.out.println("Hat Color: " + papahat);
+String smurfetteIsGirlOrBoy = smurfettesmurf.isGirlOrBoy();
+System.out.println(smurfetteIsGirlOrBoy);
+System.out.println("--------------");
+}
 }
